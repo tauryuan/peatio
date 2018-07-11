af802d094941d0de9b11ff4c4bdb150ffc560f23
MoD <mod@websys.io>
2018-07-11 04:39:45 +0300

    Adding factories and a test stub

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,6 +93,7 @@ RSpec.configure do |config|
     KlineDB.stubs(:kline).returns([])
     I18n.locale = :en
     %i[ usd btc dash eth xrp trst bch eur ].each { |ccy| FactoryBot.create(:currency, ccy) }
+    %w[ eth-rinkeby ].each { |ccy| FactoryBot.create(:blockchain, ccy) }
     %i[ btcusd dashbtc btceth ].each { |market| FactoryBot.create(:market, market) }
   end
 
