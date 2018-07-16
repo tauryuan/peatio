580d04ecb0af5e21ca6dc7daa514c147d17cb8d0
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-07-16 13:12:05 +0300

    Revert some changes (related to #1422)

--- app/models/blockchain.rb
+++ app/models/blockchain.rb
@@ -16,14 +16,6 @@ class Blockchain < ActiveRecord::Base
   def status
     super&.inquiry
   end
-
-  def case_insensitive?
-    !case_sensitive?
-  end
-
-  def confirmations_max
-    [deposit_confirmations, withdraw_confirmations].max
-  end
 end
 
 # == Schema Information
