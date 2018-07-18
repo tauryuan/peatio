7c00c36abb4cad6803b0c6af7811040b5924fed9
Dinesh Chohda <35770481+dinesh-skyach@users.noreply.github.com>
2018-07-18 23:06:04 +0530

    Added Wallet/Blockchain validations (#1429)
    * Added Wallet/Blockchain validations

--- spec/factories/blockchains.rb
+++ spec/factories/blockchains.rb
@@ -14,5 +14,17 @@ FactoryBot.define do
       explorer_transaction    'https://etherscan.io/tx/#{txid}'
       status                  'active'
     end
+
+    trait 'eth-mainet' do
+      key                     'eth-mainet'
+      name                    'Ethereum Mainet'
+      client                  'ethereum'
+      server                  'http://127.0.0.1:8545'
+      height                  2500000
+      min_confirmations       4
+      explorer_address        'https://etherscan.io/address/#{address}'
+      explorer_transaction    'https://etherscan.io/tx/#{txid}'
+      status                  'disabled'
+    end
   end
 end
